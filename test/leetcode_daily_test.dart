6622bc8bee97a1c91ee2427f8fb4b383a457ed5c
Rui Alves <ruialves99k@gmail.com>
2025-03-03 11:28:05 +0100

    add daily exercise

--- test/leetcode_daily_test.dart
+++ test/leetcode_daily_test.dart
@@ -1,6 +1,7 @@
 import 'package:leetcode/exercises/add_two_numbers.dart';
 import 'package:leetcode/exercises/daily/2025/03/daily20250301.dart';
 import 'package:leetcode/exercises/daily/2025/03/daily20250302.dart';
+import 'package:leetcode/exercises/daily/2025/03/daily20250303.dart';
 import 'package:leetcode/models/list_node.dart';
 import 'package:test/test.dart';
 
@@ -20,5 +21,10 @@ void dailyExercises() {
     final daily = Daily20250302();
     expect(daily.mergeArrays([[1,2],[2,3],[4,5]], [[1,4],[3,2],[4,1]]), [[1,6],[2,3],[3,2],[4,6]]);
     expect(daily.mergeArrays([[2,4],[3,6],[5,5]], [[1,3],[4,3]]), [[1,3],[2,4],[3,6],[4,3],[5,5]]);
+  });
+  test('Daily 2025-03-03', (){
+    final daily = Daily20250303();
+    expect(daily.pivotArray([9,12,5,10,14,3,10], 10), [9,5,3,10,10,12,14]);
+    expect(daily.pivotArray([-3,4,3,2], 2), [-3,2,4,3]);
   });
 }
